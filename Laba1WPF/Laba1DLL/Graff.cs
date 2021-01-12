df81be0a81241befcc5ded9b5c78fc372b21c2c7
h4chupi77y <67737225+h4chupi77y@users.noreply.github.com>
2021-01-12 21:29:20 +0300

    Add files via upload

---
+++ Laba1WPF/Laba1DLL/Graff.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Laba1DLL
+{
+    public class Summ
+    {
+        private string a;
+        private string b;
+
+        
+
+        public int RecogOperation (string enter)
+        {
+            int recog = 0;
+            int helpint;
+            String helpstring;
+            String helpstring2;
+            for(int i=0;i<enter.Length;i++)
+            {
+                if (enter[i] == '+')
+                {
+                    helpint = i;
+                    break;
+                }
+            }
+            
+
+            return recog;
+        }
+    }
+}
